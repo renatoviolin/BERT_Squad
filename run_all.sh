6527bc9d3e2ebcf8aad63e50ae5dad6d646769ad
Renato Violin <renato@MacBook-Pro-de-Renato.local>
2019-04-06 01:52:09 -0300

    Rull All

--- run_all.sh
+++ run_all.sh
@@ -3,17 +3,17 @@ rm output_v2/*
 python evaluate-v2.0.py squad_min/dev-v2.0-min.json output_v2/predictions.json > eval/linear_dropout
 
 rm output_v2/*
-nohup ./train2_relu_drop.sh
+./train2_relu_drop.sh
 python evaluate-v2.0.py squad_min/dev-v2.0-min.json output_v2/predictions.json > eval/relu_dropout
 
 rm output_v2/*
-nohup ./train2_tanh_drop.sh
+./train2_tanh_drop.sh
 python evaluate-v2.0.py squad_min/dev-v2.0-min.json output_v2/predictions.json > eval/tanh_dropout
 
 rm output_v2/*
-nohup ./train2_tanh_nodrop.sh
+./train2_tanh_nodrop.sh
 python evaluate-v2.0.py squad_min/dev-v2.0-min.json output_v2/predictions.json > eval/tanh_nodropout
 
 rm output_v2/*
-nohup ./train2_relu_nodrop.sh
+./train2_relu_nodrop.sh
 python evaluate-v2.0.py squad_min/dev-v2.0-min.json output_v2/predictions.json > eval/relu_nodropout
