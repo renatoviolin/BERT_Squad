66cffcff18b91e5f81b63f6b6f24520a9862f125
Renato Violin <renato@MacBook-Pro-de-Renato.local>
2019-04-06 01:50:29 -0300

    Rull All

--- run_all.sh
+++ run_all.sh
@@ -1,24 +1,19 @@
 rm output_v2/*
-rm nohup.out
-nohup ./train2_linear_drop.sh &
+./train2_linear_drop.sh
 python evaluate-v2.0.py squad_min/dev-v2.0-min.json output_v2/predictions.json > eval/linear_dropout
 
 rm output_v2/*
-rm nohup.out
-nohup ./train2_relu_drop.sh &
+nohup ./train2_relu_drop.sh
 python evaluate-v2.0.py squad_min/dev-v2.0-min.json output_v2/predictions.json > eval/relu_dropout
 
 rm output_v2/*
-rm nohup.out
-nohup ./train2_tanh_drop.sh &
+nohup ./train2_tanh_drop.sh
 python evaluate-v2.0.py squad_min/dev-v2.0-min.json output_v2/predictions.json > eval/tanh_dropout
 
 rm output_v2/*
-rm nohup.out
-nohup ./train2_tanh_nodrop.sh &
+nohup ./train2_tanh_nodrop.sh
 python evaluate-v2.0.py squad_min/dev-v2.0-min.json output_v2/predictions.json > eval/tanh_nodropout
 
 rm output_v2/*
-rm nohup.out
-nohup ./train2_relu_nodrop.sh &
+nohup ./train2_relu_nodrop.sh
 python evaluate-v2.0.py squad_min/dev-v2.0-min.json output_v2/predictions.json > eval/relu_nodropout
